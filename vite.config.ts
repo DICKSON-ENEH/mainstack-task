af6c25461efe2e17eb87bbc86a0190b5e7aaf9c6
dickson eneh <dickson.eneh-benefit.finance@dicksons-MacBook-Pro.local>
2025-11-02 17:08:52 +0100

    chnages

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  // test: {
-  //   environment: "jsdom",
-  //   globals: true,
-  //   setupFiles: "./src/setupTests.ts",
-  // },
+  preview: {
+    host: "0.0.0.0",
+    port: 10000,
+    strictPort: true,
+  },
 });
